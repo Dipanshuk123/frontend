6700951046dfcfb725b775e4beb823712c6bb708
Dipanshu <dipanshudee12345@gmail.com>
2022-12-14 23:34:34 +0530

    .

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,191 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { Col, Row, Container } from "@kunukn/react-bootstrap-grid";
+import DragDrop from "./DragDrop";
+import Table from "./Table";
+
+const Sidebar = () => {
+  const drawerWidth = 240;
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen
+        }),
+        marginLeft: 0
+      })
+    })
+  );
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== "open"
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    })
+  }));
+
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  }));
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div style={{ marginLeft: "30px" }}>
+        <CssBaseline />
+        <div
+          position="fixed"
+          open={open}
+          style={{ backgroundColor: "#f9fafc" }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <div>
+              <h4 style={{ marginTop: "20px" }}>
+                <span style={{ color: "#d7d4d1" }}>Keyword Explorer </span>{" "}
+                {">"} Keyword overview
+              </h4>
+              <h4 style={{ textAlign: "left" }}>
+                Keyword: Shopping in barcelona
+                <br />
+                Database: United States
+              </h4>
+            </div>
+            <hr />
+          </Toolbar>
+        </div>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box"
+            }
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {["Dashboard", "Recipes"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? (
+                      <InboxIcon />
+                    ) : index === 1 ? (
+                      <MailIcon />
+                    ) : (
+                      "h"
+                    )}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {[
+              "Blog",
+              "Template",
+              "Favorites",
+              "Custom Template",
+              "Integrations",
+              "Semrush"
+            ].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <hr className="hr" />
+        <div>
+          <Table />
+        </div>
+        <div style={{ width: "200px" }}>
+          <DragDrop />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Table.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import DataTable from "react-data-table-component"
+import { useState, useEffect } from "react"
+
+const Table = ()=> {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [perPage, setPerPage] = useState(10)
+
+  const columns = [
+    {
+      name: "User ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Completed",
+      selector: (row) => (row.completed ? "Yes" : "No"),
+    },
+  ]
+
+  useEffect(() => {
+    fetchTableData()
+  }, [])
+
+  async function fetchTableData() {
+    setLoading(true)
+    const URL = "https://jsonplaceholder.typicode.com/todos"
+    const response = await fetch(URL)
+
+    const users = await response.json()
+    setData(users)
+    setLoading(false)
+  }
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <button className="list">Add to List</button>
+      <DataTable
+        columns={columns}
+        data={data}
+        progressPending={loading}
+        pagination
+      />
+    </div>
+  )
+}
+export default Table
